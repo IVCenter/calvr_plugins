ec79ce6dd8134272b96f558bc01e78aa1c5a97eb
John Mangan <jmangan@ucsd.edu>
2011-07-29 13:34:06 -0700

    Removed some commented lines.

--- calit2/GreenLight/GreenLight.cpp
+++ calit2/GreenLight/GreenLight.cpp
@@ -429,36 +429,11 @@ void GreenLight::postFrame()
 
 bool GreenLight::keyEvent(bool keyDown, int key, int mod)
 {
-//    std::cerr << "GreenLight keyEvent: keyDown: " << keyDown << " key: " << key << " char: " << (char)key << " mod: " << mod << std::endl;
     return false;
 }
 
 bool GreenLight::buttonEvent(int type, int button, int hand, const osg::Matrix& mat)
 {
-/*
-    std::cerr << "Button event type: ";
-    switch(type)
-    {
-        case BUTTON_DOWN:
-            std::cerr << "BUTTON_DOWN ";
-            break;
-        case BUTTON_UP:
-            std::cerr << "BUTTON_UP ";
-            break;
-        case BUTTON_DRAG:
-            std::cerr << "BUTTON_DRAG ";
-            break;
-        case BUTTON_DOUBLE_CLICK:
-            std::cerr << "BUTTON_DOUBLE_CLICK ";
-            break;
-        default:
-            std::cerr << "UNKNOWN ";
-            break;
-    }
-
-    std::cerr << "hand: " << hand << " button: " << button << std::endl;
-*/
-
     if (type != cvr::BUTTON_DOWN || button != 0)
         return false;
 
@@ -488,49 +463,11 @@ bool GreenLight::buttonEvent(int type, int button, int hand, const osg::Matrix& 
         return true;
     }
 
-/* no longer need this.. commenting out instead of deletion, until tested
-    // process intersection
-    osg::Vec3 pointerStart, pointerEnd;
-    std::vector<IsectInfo> isecvec;
-
-    pointerStart = mat.getTrans();
-    pointerEnd.set(0.0f, 10000.0f, 0.0f);
-    pointerEnd = pointerEnd * mat;
-
-    isecvec = getObjectIntersection(cvr::PluginHelper::getScene(),
-                pointerStart, pointerEnd);
-
-    if (isecvec.size() > 0)
-        return handleIntersection(isecvec[0].geode);
-*/
     return false;
 }
 
 bool GreenLight::mouseButtonEvent(int type, int button, int x, int y, const osg::Matrix& mat)
 {
-/*
-    std::cerr << "Mouse Button event type: ";
-    switch(type)
-    {
-        case MOUSE_BUTTON_DOWN:
-            std::cerr << "MOUSE_BUTTON_DOWN ";
-            break;
-        case MOUSE_BUTTON_UP:
-            std::cerr << "MOUSE_BUTTON_UP ";
-            break;
-        case MOUSE_DRAG:
-            std::cerr << "MOUSE_DRAG ";
-            break;
-        case MOUSE_DOUBLE_CLICK:
-            std::cerr << "MOUSE_DOUBLE_CLICK ";
-            break;
-        default:
-            std::cerr << "UNKNOWN ";
-            break;
-    }
-
-    std::cerr << "button: " << button << std::endl;
-*/
     // Left Button Click
     if (type != cvr::MOUSE_BUTTON_DOWN || button != 0)
         return false;
@@ -561,20 +498,5 @@ bool GreenLight::mouseButtonEvent(int type, int button, int x, int y, const osg:
         return true;
     }
 
-/* no longer need this.. commenting out instead of deletion, until tested
-    // process mouse intersection
-    osg::Vec3 pointerStart, pointerEnd;
-    std::vector<IsectInfo> isecvec;
-
-    pointerStart = mat.getTrans();
-    pointerEnd.set(0.0f, 10000.0f, 0.0f);
-    pointerEnd = pointerEnd * mat;
-
-    isecvec = getObjectIntersection(cvr::PluginHelper::getScene(),
-                pointerStart, pointerEnd);
-
-    if (isecvec.size() > 0)
-        return handleIntersection(isecvec[0].geode);
-*/
     return false;
 }
