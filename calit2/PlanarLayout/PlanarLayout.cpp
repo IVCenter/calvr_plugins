ca3e43423a97d24629e954e49650994e1e135d1d
John Mangan <jmangan@eng.ucsd.edu>
2013-06-18 12:57:23 -0700

    * Created the PlanarLayout (animated) plugin.

---
+++ calit2/PlanarLayout/PlanarLayout.cpp
@@ -0,0 +1,182 @@
+#include "PlanarLayout.h"
+
+#include <cvrKernel/SceneManager.h>
+#include <cvrKernel/PluginHelper.h>
+#include <PluginMessageType.h>
+
+#include <iostream>
+#include <cmath>
+
+using namespace osg;
+using namespace std;
+using namespace cvr;
+
+CVRPLUGIN(PlanarLayout)
+
+bool PlanarLayout::init()
+{
+    cerr << "PlanarLayout init" << endl;
+
+    if (SceneManager::instance()->getTiledWallValid())
+    {
+        mLayoutAlgorithm = new PlanarLayoutAlgorithm;
+
+        LayoutManagerAddLayoutData lmald;
+        lmald.layout = mLayoutAlgorithm;
+
+        PluginHelper::sendMessageByName( "LayoutManager", LM_ADD_LAYOUT, (char*)&lmald);
+    }
+
+    cerr << "PlanarLayout init done.\n";
+
+    return SceneManager::instance()->getTiledWallValid();
+}
+
+
+PlanarLayout::~PlanarLayout()
+{
+    delete mLayoutAlgorithm;
+}
+
+/*virtual*/ string
+PlanarLayout::PlanarLayoutAlgorithm::Name( void )
+{
+    return "Planar Layout";
+}
+
+/*virtual*/ void
+PlanarLayout::PlanarLayoutAlgorithm::Cleanup( void )
+{
+    mObjTrans.clear();
+}
+
+/*virtual*/ bool
+PlanarLayout::PlanarLayoutAlgorithm::Start( void )
+{
+    mTimeElapsed = 0.0;
+    mObjTrans.clear();
+
+    vector< SceneObject* > movable_objects;
+
+    {
+        vector< SceneObject* > scene_objects = SceneManager::instance()->getSceneObjects();
+
+        for (unsigned int i = 0; i < scene_objects.size(); ++i)
+        {
+            if (scene_objects[i]->getMovable())
+                movable_objects.push_back( scene_objects[i] );
+        }
+
+        if (movable_objects.empty())
+            return false;
+    }
+
+    unsigned int cols = (unsigned int)ceil( sqrt(movable_objects.size()) * SceneManager::instance()->getTiledWallWidth() / (double)SceneManager::instance()->getTiledWallHeight() );
+    unsigned int rows = (unsigned int)ceil(movable_objects.size() / (double)cols);
+
+    while (GetBestGrid(rows, cols, movable_objects.size()))
+        ;
+
+    double x_inc = SceneManager::instance()->getTiledWallWidth() / cols;
+    double y_inc = SceneManager::instance()->getTiledWallHeight() / rows;
+
+    unsigned int m = 0;
+
+    for (unsigned int r = 0; r < rows && m < movable_objects.size(); ++r)
+    {
+        double pos_z = SceneManager::instance()->getTiledWallHeight() / 2.0
+                    - (y_inc / 2.0) - (r * y_inc);
+
+        for (unsigned int c = 0; c < cols && m < movable_objects.size(); ++c)
+        {
+            double pos_x = SceneManager::instance()->getTiledWallWidth() / -2.0
+                        + (x_inc / 2.0) + (c * x_inc);
+
+            TransitionState trans;
+            trans.startPos = movable_objects[m]->getPosition();
+            trans.endPos = Vec3(pos_x, 0, pos_z);
+            trans.endPos = SceneManager::instance()->getTiledWallTransform() * trans.endPos;
+
+            mObjTrans[ movable_objects[m] ] = trans;
+            ++m;
+        }
+    }
+
+    return true;
+}
+
+/*virtual*/ bool
+PlanarLayout::PlanarLayoutAlgorithm::Update( void )
+{
+    double const TOTAL_TIME = 3.0;
+
+    mTimeElapsed += PluginHelper::getLastFrameDuration();
+
+    if (mTimeElapsed > TOTAL_TIME)
+        mTimeElapsed = TOTAL_TIME;
+
+    vector< SceneObject* > scene_objects = SceneManager::instance()->getSceneObjects();
+
+    for (unsigned int i = 0; i < scene_objects.size(); ++i)
+    {
+        SceneObjTrans::iterator it = mObjTrans.find( scene_objects[i] );
+
+        if (mObjTrans.end() == it)
+            continue;
+
+        Vec3 pos = it->second.endPos + (it->second.startPos - it->second.endPos) * (1.0 - mTimeElapsed / TOTAL_TIME);
+
+        it->first->setPosition( pos );
+    }
+
+    return (TOTAL_TIME <= mTimeElapsed);
+}
+
+bool
+PlanarLayout::PlanarLayoutAlgorithm::GetBestGrid(unsigned int &r, unsigned int &c, unsigned int cells) const
+{
+    unsigned int best_r = r, best_c = c;
+    double best_rating = Rate(r, c, cells);
+
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j<= 1; j++)
+        {
+            if (0 == i && 0 == j)
+                continue;
+
+            double rating = Rate(r+i,c+j, cells);
+            if (rating > best_rating)
+            {
+                best_rating = rating;
+                best_r = r+i;
+                best_c = c+j;
+            }
+        }
+    }
+
+    if (best_r == r && best_c == c)
+        return false;
+
+    r = best_r;
+    c = best_c;
+    return true;
+}
+
+double
+PlanarLayout::PlanarLayoutAlgorithm::Rate(unsigned int r, unsigned int c, unsigned int cells) const
+{
+    if (r*c < cells || r < 0 || c < 0)
+        return 0;
+
+    double ratio = (SceneManager::instance()->getTiledWallWidth() * r)
+                   / (SceneManager::instance()->getTiledWallHeight() * c);
+    if (ratio > 1)
+        ratio = 1 / ratio;
+
+    double mod = cells / (double)(r*c);
+    mod = mod * mod * mod;
+
+    return ratio * mod;
+}
+
