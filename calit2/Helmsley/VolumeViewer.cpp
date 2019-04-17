947a932eedf2099ae10593ca35668ab730c77c51
ZhangMenghe <zmh.leaves@gmail.com>
2019-04-16 18:48:40 -0700

    add helmsley

--- calit2/Helmsley/VolumeViewer.cpp
+++ calit2/Helmsley/VolumeViewer.cpp
@@ -1,50 +1,21 @@
 #include <cvrUtil/AndroidStdio.h>
-#include <osg/LineWidth>
-#include <osgUtil/Tessellator>
-#include <osg/Texture>
-#include <osg/Texture2D>
-#include <cvrUtil/AndroidHelper.h>
+#include <cvrMenu/MenuSystem.h>
 #include <osg/ShapeDrawable>
-#include <cvrKernel/PluginManager.h>
-
-#include "VolumeViewer"
+#include <cvrUtil/AndroidHelper.h>
+#include "VolumeViewer.h"
 using namespace osg;
 using namespace cvr;
 
-bool VolumeViewer:: tackleHitted(osgUtil::LineSegmentIntersector::Intersection result ){
-    return true;
-}
-
 void VolumeViewer::initMenuButtons() {
-
-    //_lightButton = new MenuCheckbox("Add Light Source", false);
-    //_lightButton->setCallback(this);
-    //_mainMenu->addItem(_lightButton);
 }
 
 bool VolumeViewer::init() {
     // --------------- create the menu ---------------
-    _mainMenu = new SubMenu("GlesDrawable", "GlesDrawable");
+    _mainMenu = new SubMenu("Helmsley", "VolumeViewer");
     _mainMenu->setCallback(this);
     MenuSystem::instance()->addMenuItem(_mainMenu);
     initMenuButtons();
 
-    _root = new Group;
-    _objects = new Group;
-
-    SceneManager::instance()->getSceneRoot()->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
-
-    //bool navigation, bool movable, bool clip, bool contextMenu, bool showBounds
-    rootSO= new SceneObject("glesRoot", false, false, false, false, false);
-    rootSO->addChild(_root);
-    objSO = new SceneObject("testBoundObj", false, false, false, false, true);
-    rootSO->addChild(objSO);
-    objSO->addChild(_objects);
-    objSO->dirtyBounds();
-
-    PluginHelper::registerSceneObject(rootSO, "VolumeViewer Plugin");
-    rootSO->dirtyBounds();
-    rootSO->attachToScene();
 
     return true;
 }
@@ -52,73 +23,6 @@ bool VolumeViewer::init() {
 void VolumeViewer::menuCallback(cvr::MenuItem *item) {}
 
 void VolumeViewer::postFrame() {
-    /*_pointcloudDrawable->updateOnFrame();
-    cvr::planeMap map = ARCoreManager::instance()->getPlaneMap();
-    if(_plane_num < map.size()){
-        for(int i= _plane_num; i<map.size();i++){
-            planeDrawable * pd = new planeDrawable();
-            _root->addChild(pd->createDrawableNode());
-            _planeDrawables.push_back(pd);
-        }
-        _plane_num = map.size();
-    }
-    auto planeIt = map.begin();
-    for(int i=0; i<_plane_num; i++,planeIt++)
-        _planeDrawables[i]->updateOnFrame(planeIt->first, planeIt->second);
-
-
-    Vec3f isPoint;
-    if(TrackingManager::instance()->getIsPoint(isPoint)){
-        _strokeDrawable->updateOnFrame(isPoint);
-        _strokeDrawable->getGLNode()->setNodeMask(0xFFFFFF);
-    } else
-        _strokeDrawable->getGLNode()->setNodeMask(0x0);
-
-
-    size_t anchor_num = ARCoreManager::instance()->getAnchorSize();
-    if( anchor_num != 0){
-        if(_objNum < anchor_num){
-            for(int i=_objNum; i<anchor_num; i++){
-                Matrixf modelMat;
-                if(!ARCoreManager::instance()->getAnchorModelMatrixAt(modelMat, i))
-                    break;
-                if(_add_light){
-                    createDebugSphere(_objects, modelMat);
-//
-                    osg::Vec3f debug = modelMat.getTrans();
-
-                    osg::Vec4f tmp = osg::Vec4f(debug.x(), debug.z(), -debug.y(),1.0) * (*ARCoreManager::instance()->getViewMatrix());
-                    _lightPosition = osg::Vec3f(tmp.x() / tmp.w(), -tmp.z()/tmp.w(), tmp.y()/tmp.w());
-                            LOGE("===LIGHT: %f, %f, %f", _lightPosition.x(), _lightPosition.y(), _lightPosition.z());
-                    _add_light = false;
-                    break;
-                }
-                switch(last_object_select){
-                    case 1:
-                        createObject(_objects,"models/andy.obj", "textures/andy.png",
-                                     modelMat, ONES_SOURCE);
-                        break;
-                    case 2:
-                        createObject(_objects,"models/andy.obj", "textures/andy.png",
-                                     modelMat, ONES_SOURCE);
-                        break;
-                    case 3:
-                        createObject(_objects,"models/andy.obj", "textures/andy.png",
-                                     modelMat, SPHERICAL_HARMONICS);
-                        break;
-                    default:
-                        createObject(_objects,"models/andy.obj", "textures/andy.png",
-                                     modelMat, ARCORE_CORRECTION);
-                }
-
-            }
-
-        }
-        _objNum = anchor_num;
-    }
-//    _quadDrawable->updateOnFrame(ARCoreManager::instance()->getCameraTransformedUVs());
-*/
-
 }
 
 bool VolumeViewer::processEvent(cvr::InteractionEvent * event){
--- calit2/Helmsley/VolumeViewer.h
+++ calit2/Helmsley/VolumeViewer.h
@@ -1,35 +1,11 @@
 #ifndef VOLUME_VIEWER_H
 #define VOLUME_VIEWER_H
 
-// STD
-#include <queue>
-#include <vector>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-// CVR
 #include <cvrKernel/CVRPlugin.h>
-#include <cvrKernel/ScreenBase.h>
-#include <cvrKernel/SceneManager.h>
-#include <cvrKernel/SceneObject.h>
-#include <cvrKernel/Navigation.h>
-#include <cvrKernel/PluginHelper.h>
-#include <cvrKernel/ComController.h>
-#include <cvrMenu/MenuSystem.h>
+#include <cvrMenu/MenuItem.h>
+#include <osg/MatrixTransform>
 #include <cvrMenu/SubMenu.h>
-#include <cvrMenu/MenuButton.h>
-#include <cvrMenu/MenuCheckbox.h>
-#include <cvrMenu/MenuRangeValue.h>
-#include <cvrConfig/ConfigManager.h>
-#include <cvrKernel/FileHandler.h>
-#include <cvrUtil/AndroidHelper.h>
-// OSG
-#include <osg/Group>
-#include <osg/Vec3>
-#include <osg/Uniform>
-#include <osgDB/ReadFile>
-#include <osgDB/FileUtils>
-
 
 class VolumeViewer : public cvr::CVRPlugin, public cvr::MenuCallback
 {
@@ -38,14 +14,9 @@ typedef osg::ref_ptr<osg::MatrixTransform> Transform;
 protected:
     cvr::SubMenu *_mainMenu;
 
-    osg::Group *_root, *_objects;
-    cvr::SceneObject *rootSO, *objSO;
-
     void initMenuButtons();
     void createDebugSphere(osg::Group *, osg::Matrixf);
-    //void createObject(osg::Group *, const char*, const char*, osg::Matrixf, LightingType);
-    //void createObject(osg::Group *, const char*, const char*, osg::Matrixf, bool opengl);
-    bool tackleHitted(osgUtil::LineSegmentIntersector::Intersection result );
+
 public:
     bool init();
     void menuCallback(cvr::MenuItem * item);
